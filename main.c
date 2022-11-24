69ae12f9146535d16b1065fd3cd55ce21606bbde
mokshshah18 <acams1803@gmail.com>
2022-11-23 23:39:31 -0500

    Changing computers

--- main.c
+++ main.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
+#include <json-c
+
+int employeeVals(employeeId){
+	int employeeCount=sizeof(employeeId)/sizeof(employeeId[0]);
+	for (int i = 0; i < employeeCount; i++) {     
+        printf("%d ", employeeId[i]);     
+    	}   
+}
 
 int main(){
-	printf("Base fefsdfiledf:n\n");
+	int* employeeId;
+	employeeId = calloc(10, sizeof(int));
+	//={1,2,3,4,5,6,7,8,9,10};
+	int employeeCount=sizeof(employeeId)/sizeof(employeeId[0]);
+	int option;
+	printf("1: Look at all employees\n"); // should show employee, their department, their emails and months in company
+	scanf("%d",&option);
+	if(option==1){
+		printf("Now showing all employee data:\n");
+		for (int i = 0; i < employeeCount; i++) {     
+        printf("%d ", employeeId[i]);     
+    	}   
+	}
+	free(employeeId);
+	return 0;
 }
